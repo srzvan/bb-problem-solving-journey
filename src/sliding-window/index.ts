2f303274a9ce26d292483034143a9642f2cff37b
Răzvan Sbîngu <razvan.sbingu@thinslices.com>
2024-03-15 12:57:15 +0200

    sliding-window practice aka razwan saved the day 🦸

--- src/sliding-window/index.ts
+++ src/sliding-window/index.ts
@@ -1,4 +1,76 @@
-export function slidingWindow(str: string) {
+/*
+  Se dă string-ul "110110011".
+  Care este lungimea celui mai lung
+    substring care conține numai "1" și
+    îl putem obține făcând flip la un singur "0"?
+*/
+
+export function longestBinarySubstring(str: string) {
   console.log(str);
-  return "coco";
+  throw new Error("Not implemented");
+}
+
+/*
+  input: nums [1, 2, 5, 6, 7]
+  k = 15
+
+  What is the length of the longest
+    sub-array where the product
+    of its elements is less than k?
+*/
+
+export function maxLen(nums: number[], compare: number) {
+  let start = 0;
+  let maxLen = 0;
+  let product = 1;
+
+  for (let end = 0; end < nums.length; end++) {
+    if (nums[end] === 0) {
+      return nums.length;
+    }
+
+    product *= nums[end];
+
+    while (product >= compare) {
+      product /= nums[start];
+      start++;
+    }
+
+    maxLen = Math.max(maxLen, end - start + 1);
+  }
+
+  return maxLen;
+}
+
+/*
+  input: nums [10, 5, 2, 6]
+  k = 100
+
+  What is the number of subarrays
+    where the product of all the elements
+    is less than k?
+*/
+export function numOfSubarrays(nums: number[], compare: number) {
+  let start = 0;
+  let count = 0;
+  let product = 1;
+
+  for (let end = 0; end < nums.length; end++) {
+    if (nums[end] === 0) {
+      return (nums.length * (nums.length + 1)) / 2;
+    }
+
+    product *= nums[end];
+
+    while (product >= compare) {
+      product /= nums[start];
+      start++;
+    }
+
+    let length = end - start + 1;
+
+    count += length;
+  }
+
+  return count;
 }
--- src/sliding-window/sliding-window.test.ts
+++ src/sliding-window/sliding-window.test.ts
@@ -1,8 +1,53 @@
-import { slidingWindow } from ".";
+import { longestBinarySubstring, maxLen, numOfSubarrays } from ".";
 
-describe("sliding-window", () => {
+describe("longestBinarySubstring", () => {
   it("should work", () => {
-    const result = slidingWindow("coco");
+    const result = longestBinarySubstring("coco");
     expect(result).toEqual("coco");
+  });
+});
+
+describe("max-len", () => {
+  it("should work", () => {
+    const result = maxLen([1, 2, 5, 6, 7], 15);
+    expect(result).toEqual(3);
+
+    const result1 = maxLen([1, 2, 3, 4, 5, 6, 7], 181);
+    expect(result1).toEqual(5);
+
+    const result3 = maxLen([1, 2, 3, 4, 5, 6, 7], 0);
+    expect(result3).toEqual(0);
+  });
+
+  it("array contains 0", () => {
+    const result = maxLen([0, 1, 2, 3, 4, 5, 6, 7], 5);
+    expect(result).toEqual(8);
+
+    const result1 = maxLen([1, 2, 3, 4, 5, 6, 7, 0], 5);
+    expect(result1).toEqual(8);
+
+    const result3 = maxLen([1, 2, 3, 4, 0, 6, 7, 5], 5);
+    expect(result3).toEqual(8);
+  });
+});
+
+describe("numOfSubarrays", () => {
+  it("should work", () => {
+    const result = numOfSubarrays([10, 5, 2, 6], 100);
+    expect(result).toEqual(8);
+
+    const result1 = numOfSubarrays([1, 5, 2, 6], 3);
+    expect(result1).toEqual(2);
+  });
+
+  it("array contains 0", () => {
+    const result = numOfSubarrays([1, 5, 2, 6, 0], 3);
+    expect(result).toEqual(15);
+
+    const result1 = numOfSubarrays([1, 5, 0, 6, 2], 3);
+    expect(result1).toEqual(15);
+
+    const result2 = numOfSubarrays([0, 1, 5, 2, 6], 3);
+    expect(result2).toEqual(15);
   });
 });
